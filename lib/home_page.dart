e8a5aa18d55169e2fbae6d3eb413192b054f4fe7
Fabio <josefabiosan@gmail.com>
2023-08-09 22:45:27 -0300

    criado arquivo home_page

---
+++ lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var numberoGerado = 0;
+
+  int _numberoAleatorio() {
+    Random gerarNumero = Random();
+    return gerarNumero.nextInt(1000);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "Nico",
+                textAlign: TextAlign.right,
+              ),
+            ],
+          ),
+        ),
+        body: Center(child: Text(numberoGerado.toString())),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              numberoGerado = _numberoAleatorio();
+            });
+          },
+        ));
+  }
+}
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+import 'home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,46 +14,5 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var numberoGerado = 0;
-
-  int _numberoAleatorio() {
-    Random gerarNumero = Random();
-    return gerarNumero.nextInt(1000);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                "Nico",
-                textAlign: TextAlign.right,
-              ),
-            ],
-          ),
-        ),
-        body: Center(child: Text(numberoGerado.toString())),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              numberoGerado = _numberoAleatorio();
-            });
-          },
-        ));
   }
 }
