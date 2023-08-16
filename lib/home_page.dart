82e1d014200f7f0ab01c34f47c5232a7d59fb503
Fabio <josefabiosan@gmail.com>
2023-08-15 21:31:53 -0300

    Layout row flex container

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -29,35 +29,77 @@ class _HomePageState extends State<HomePage> {
         width: double.infinity,
         margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Center(
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Colors.amberAccent,
                 child: Text(
-              'Gerando $quantidadeCliques vezes',
-              style: GoogleFonts.aboreto(fontSize: 15),
-            )),
-            Center(
+                  'Ações do usuário',
+                  style: GoogleFonts.acme(fontSize: 15),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                width: double.infinity,
+                color: Colors.orange,
+                child: Text(
+                  'Gerando $quantidadeCliques vezes',
+                  style: GoogleFonts.adamina(fontSize: 15),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              color: Colors.lightGreen,
               child: Text(
                 'Numero da sorte: $numeroGerado',
                 style: GoogleFonts.aboreto(fontSize: 20),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Start',
-                  style: GoogleFonts.aboreto(fontSize: 20),
+            Expanded(
+              child: Container(
+                color: Colors.grey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.yellow,
+                        child: Text(
+                          'Nome:',
+                          style: GoogleFonts.acme(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        color: Colors.red,
+                        child: Text(
+                          'Fabio Ventura',
+                          style: GoogleFonts.acme(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.green,
+                        child: Text(
+                          'R',
+                          style: GoogleFonts.acme(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  'center',
-                  style: GoogleFonts.aboreto(fontSize: 20),
-                ),
-                Text(
-                  'end',
-                  style: GoogleFonts.aboreto(fontSize: 20),
-                ),
-              ],
+              ),
             ),
           ],
         ),
