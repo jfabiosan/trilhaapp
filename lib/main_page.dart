9b9cd2114959f32ce8092739e43ddaa6b19ccd9a
Fabio <josefabiosan@gmail.com>
2023-09-07 16:35:45 -0300

    Colocano menu Drawer

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -10,6 +10,20 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Main Page"),
+        ),
+        drawer: const Drawer(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Text("Dados Cadastrais")],
+          ),
+        )),
+      ),
+    );
   }
 }
